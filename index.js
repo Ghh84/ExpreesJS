f0b531c14cb3e862eda3e02106450f2cf9d622d1
Ghh84 <ghiwethh@gmail.com>
2024-02-26 11:20:06 +0200

    geting users using api

--- index.js
+++ index.js
@@ -1,13 +1,27 @@
-const express=require('express')
+const express = require('express');
 const app = express();
 const path=require('path')
-// app.get('/', (req,res)=>{
-//     res.send('<h1>Tervetullo Vantaalle!</h1>')
-// });
-
-//sendig static folder
+const users=require('./users')
+const logger=require('./middleware/logger')
+//init middleware
+app.use(logger)
+// get all users
+app.get('/api/users', (req,res)=> res.json(users));
+//get single user
+app.get('/api/users/:id', (req,res)=>{
+    const found=users.some(user=>user.id===parseInt(req.params.id));
+    if(found){
+        res.json(users.filter(user=>user.id===parseInt(req.params.id)))
+    }
+    else{
+        res.status(400).json(`No user with the Id ${req.params.id}`)
+    }
+    
+})
+//get static folder
 app.use(express.static(path.join(__dirname,'dist')))
 
 const PORT=process.env.PORT || 5000;
 
-app.listen(PORT,()=>console.log(`Server is not response at port ${PORT}`))
+
+app.listen(PORT,()=>console.log(`Server is response at port ${PORT}`))
