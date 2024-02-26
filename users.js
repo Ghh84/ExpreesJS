15d3b1e512455534b60696f7961f1e184800bf89
Ghh84 <ghiwethh@gmail.com>
2024-02-26 11:18:50 +0200

    Add users file

---
+++ users.js
@@ -0,0 +1,26 @@
+const users=[{
+    id:1,
+    name:'Ghebrehiwet Hagos',
+    email:'mfigheha@murata.com',
+    status:'Active'
+},
+{
+    id:2,
+    name:'Rahsi Ghebrehiwet',
+    email:'rahsi@microsoft.com',
+    status:'Active'
+},
+{
+    id:3,
+    name:'Abel daniel',
+    email:'abelDaniel@gmail.com',
+    status:'unactive'
+},
+{
+    id:4,
+    name:'Markko lainen',
+    email:'markko@yahoo.com',
+    status:'Active'
+}];
+
+module.exports=users;
