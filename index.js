5fd94e3d7faa6ce0ab7f70ec5b856d446c35e0a0
Ghh84 <ghiwethh@gmail.com>
2024-02-27 12:45:14 +0200

    Code reviews

--- index.js
+++ index.js
@@ -1,19 +1,37 @@
 const express = require('express');
+const path = require('path');
+const exphbs = require('express-handlebars');
+const logger = require('./middleware/logger');
+const userApi=require('./route/api/usersApi')
+const users= require('./users');
+
 const app = express();
-const path=require('path');
-const logger=require('./middleware/logger');
-const usersApi=require('./route/api/usersApi')
-//init middleware
-app.use(logger)
-// body parser middleware
-app.use(express.json())
-app.use(express.urlencoded({extended:false}))
-//get static folder
-app.use(express.static(path.join(__dirname,'dist')))
 
-//get userApi
-app.use('/api/users',usersApi)
+// Init middleware
+// app.use(logger);
 
-const PORT=process.env.PORT || 5000;
+// Handlebars Middleware
+app.engine('.hbs', exphbs.engine({ extname: '.hbs', defaultLayout: "main"}));
+app.set('view engine', 'handlebars');
 
-app.listen(PORT,()=>console.log(`Server is response at port ${PORT}`))
+// Body Parser Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// Homepage Route
+app.get('/', (req, res) =>
+  res.render('index', {
+    title: 'User App',
+    users
+  })
+);
+
+// Set static folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Members API Routes
+app.use('/api/users', userApi);
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
