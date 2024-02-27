b3f32c0085e9c65f99863ad02b3c9c729dd723b4
Ghh84 <ghiwethh@gmail.com>
2024-02-27 11:27:55 +0200

    Consume users Api

--- index.js
+++ index.js
@@ -1,27 +1,19 @@
 const express = require('express');
 const app = express();
-const path=require('path')
-const users=require('./users')
-const logger=require('./middleware/logger')
+const path=require('path');
+const logger=require('./middleware/logger');
+const usersApi=require('./route/api/usersApi')
 //init middleware
 app.use(logger)
-// get all users
-app.get('/api/users', (req,res)=> res.json(users));
-//get single user
-app.get('/api/users/:id', (req,res)=>{
-    const found=users.some(user=>user.id===parseInt(req.params.id));
-    if(found){
-        res.json(users.filter(user=>user.id===parseInt(req.params.id)))
-    }
-    else{
-        res.status(400).json(`No user with the Id ${req.params.id}`)
-    }
-    
-})
+// body parser middleware
+app.use(express.json())
+app.use(express.urlencoded({extended:false}))
 //get static folder
 app.use(express.static(path.join(__dirname,'dist')))
 
-const PORT=process.env.PORT || 5000;
+//get userApi
+app.use('/api/users',usersApi)
 
+const PORT=process.env.PORT || 5000;
 
 app.listen(PORT,()=>console.log(`Server is response at port ${PORT}`))
